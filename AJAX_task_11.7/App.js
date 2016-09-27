8784e18a071497848c704527044337a223942d65
martac09 <czudak.marta@gmail.com>
2016-09-27 08:53:33 +0200

    ajax 11.7 task

---
+++ AJAX_task_11.7/App.js
@@ -0,0 +1,41 @@
+// OGÓLNA FUNKCJA
+function randomString() {
+	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
+	var str = '', i;
+	for (i = 0; i < 10; i++) {
+	  str += chars[Math.floor(Math.random() * chars.length)];
+	}
+	return str;
+}
+
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': 52,
+  'X-Auth-Token': 'fe44381f6d393c1308bf794b45415658'
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+$.ajax({
+	url: baseUrl + "/board",
+	method: 'GET',
+	success: function(response) {
+		setupColumns(response.columns);
+	}
+});
+
+function setupColumns(columns) {
+	columns.forEach(function(column) {
+		var col = new Column(column.id, column.name);
+		board.createColumn(col);
+		setupCards(col, column.cards);
+	})
+}
+function setupCards(col, cards) {
+	cards.forEach(function(card) {
+		var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+		col.createCard(card);
+	})
+}
